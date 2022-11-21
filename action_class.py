012134bc011fc690276b1aa2345c9bb470d1e39f
joyi012 <joyipsum012@outlook.com>
2022-11-21 15:33:10 +0530

    add action class

---
+++ action_class.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.execute_script("window.scrollBy(0,1000)", "")
+action = ActionChains(driver)
+action.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
+action.move_to_element(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
